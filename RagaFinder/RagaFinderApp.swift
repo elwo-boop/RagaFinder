cc561896fe17328a3e1bf221b6826fc0072375f4
Rakshay N <rakshay2108@gmail.com>
2023-06-11 12:54:59 -0400

    Initial Commit

---
+++ RagaFinder/RagaFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RagaFinderApp.swift
+//  RagaFinder
+//
+//  Created by Rakshay Narayanan on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RagaFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
